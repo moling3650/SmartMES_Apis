09117838b012885895d1c6ed374fd7927819be94
moling <365024424@qq.com>
2018-10-24 10:03:35 +0800

    添加API文档

--- SmartMES_Apis/Startup.cs
+++ SmartMES_Apis/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace SmartMES_Apis
 {
@@ -24,6 +25,10 @@ namespace SmartMES_Apis
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info { Title = "Smart.MES API", Version = "v1" });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -34,7 +39,11 @@ namespace SmartMES_Apis
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Smart.MES V1");
+            });
             app.UseMvc();
         }
     }
