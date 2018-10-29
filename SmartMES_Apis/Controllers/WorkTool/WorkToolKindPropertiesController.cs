81da2d4de3a0146dbc471dd3d6136c42b03784a1
moling <365024424@qq.com>
2018-10-29 14:50:58 +0800

    添加工装模具的API

---
+++ SmartMES_Apis/Controllers/WorkTool/WorkToolKindPropertiesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.WorkTool
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class WorkToolKindPropertiesController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public WorkToolKindPropertiesController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/WorkToolKindProperties
+        [HttpGet]
+        public IEnumerable<BWorkToolKindProperty> GetBWorkToolKindProperty([FromQuery] string modelCode)
+        {
+            if (!String.IsNullOrWhiteSpace(modelCode))
+            {
+                int? kindId = _context.BWorkToolModel.Where(item => item.ModelCode.Equals(modelCode)).FirstOrDefault()?.KindId;
+                if (kindId != null)
+                {
+                    return _context.BWorkToolKindProperty.Where(item => item.KindId == kindId);
+                }
+                else
+                {
+                    return new List<BWorkToolKindProperty>();
+                }
+            }
+            return _context.BWorkToolKindProperty;
+        }
+
+        // GET: api/WorkToolKindProperties/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBWorkToolKindProperty([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bWorkToolKindProperty = await _context.BWorkToolKindProperty.FindAsync(id);
+
+            if (bWorkToolKindProperty == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bWorkToolKindProperty);
+        }
+
+        // PUT: api/WorkToolKindProperties/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBWorkToolKindProperty([FromRoute] int id, [FromBody] BWorkToolKindProperty bWorkToolKindProperty)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bWorkToolKindProperty.PptId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bWorkToolKindProperty).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BWorkToolKindPropertyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/WorkToolKindProperties
+        [HttpPost]
+        public async Task<IActionResult> PostBWorkToolKindProperty([FromBody] BWorkToolKindProperty bWorkToolKindProperty)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BWorkToolKindProperty.Add(bWorkToolKindProperty);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBWorkToolKindProperty", new { id = bWorkToolKindProperty.PptId }, bWorkToolKindProperty);
+        }
+
+        // DELETE: api/WorkToolKindProperties/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBWorkToolKindProperty([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bWorkToolKindProperty = await _context.BWorkToolKindProperty.FindAsync(id);
+            if (bWorkToolKindProperty == null)
+            {
+                return NotFound();
+            }
+
+            _context.BWorkToolKindProperty.Remove(bWorkToolKindProperty);
+            await _context.SaveChangesAsync();
+
+            return Ok(bWorkToolKindProperty);
+        }
+
+        private bool BWorkToolKindPropertyExists(int id)
+        {
+            return _context.BWorkToolKindProperty.Any(e => e.PptId == id);
+        }
+    }
+}
---
+++ SmartMES_Apis/Controllers/WorkTool/WorkToolPropertyDetailsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.WorkTool
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class WorkToolPropertyDetailsController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public WorkToolPropertyDetailsController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/WorkToolPropertyDetails
+        [HttpGet]
+        public IEnumerable<BWorkToolPropertyDetail> GetBWorkToolPropertyDetail([FromQuery] string workToolCode)
+        {
+            if (!String.IsNullOrWhiteSpace(workToolCode))
+            {
+                return _context.BWorkToolPropertyDetail.Where(item => item.WorkToolCode.Equals(workToolCode));
+            }
+            return _context.BWorkToolPropertyDetail;
+        }
+
+        // GET: api/WorkToolPropertyDetails/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBWorkToolPropertyDetail([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bWorkToolPropertyDetail = await _context.BWorkToolPropertyDetail.FindAsync(id);
+
+            if (bWorkToolPropertyDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bWorkToolPropertyDetail);
+        }
+
+        // PUT: api/WorkToolPropertyDetails/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBWorkToolPropertyDetail([FromRoute] int id, [FromBody] BWorkToolPropertyDetail bWorkToolPropertyDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bWorkToolPropertyDetail.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bWorkToolPropertyDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BWorkToolPropertyDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/WorkToolPropertyDetails
+        [HttpPost]
+        public async Task<IActionResult> PostBWorkToolPropertyDetail([FromBody] BWorkToolPropertyDetail bWorkToolPropertyDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BWorkToolPropertyDetail.Add(bWorkToolPropertyDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBWorkToolPropertyDetail", new { id = bWorkToolPropertyDetail.Id }, bWorkToolPropertyDetail);
+        }
+
+        // DELETE: api/WorkToolPropertyDetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBWorkToolPropertyDetail([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bWorkToolPropertyDetail = await _context.BWorkToolPropertyDetail.FindAsync(id);
+            if (bWorkToolPropertyDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.BWorkToolPropertyDetail.Remove(bWorkToolPropertyDetail);
+            await _context.SaveChangesAsync();
+
+            return Ok(bWorkToolPropertyDetail);
+        }
+
+        private bool BWorkToolPropertyDetailExists(int id)
+        {
+            return _context.BWorkToolPropertyDetail.Any(e => e.Id == id);
+        }
+    }
+}
