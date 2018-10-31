319bf24a7d24b0454dcf20e141db061207493b23
moling <365024424@qq.com>
2018-10-31 18:13:15 +0800

    添加产品的API

---
+++ SmartMES_Apis/Controllers/Product/ProductTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.Product
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class ProductTypesController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public ProductTypesController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductTypes
+        [HttpGet]
+        public IEnumerable<BProductType> GetBProductType()
+        {
+            return _context.BProductType;
+        }
+
+        // GET: api/ProductTypes/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBProductType([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProductType = await _context.BProductType.FindAsync(id);
+
+            if (bProductType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bProductType);
+        }
+
+        // PUT: api/ProductTypes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBProductType([FromRoute] int id, [FromBody] BProductType bProductType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bProductType.TypeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bProductType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BProductTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductTypes
+        [HttpPost]
+        public async Task<IActionResult> PostBProductType([FromBody] BProductType bProductType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BProductType.Add(bProductType);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBProductType", new { id = bProductType.TypeId }, bProductType);
+        }
+
+        // DELETE: api/ProductTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBProductType([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProductType = await _context.BProductType.FindAsync(id);
+            if (bProductType == null)
+            {
+                return NotFound();
+            }
+
+            _context.BProductType.Remove(bProductType);
+            await _context.SaveChangesAsync();
+
+            return Ok(bProductType);
+        }
+
+        private bool BProductTypeExists(int id)
+        {
+            return _context.BProductType.Any(e => e.TypeId == id);
+        }
+    }
+}
---
+++ SmartMES_Apis/Controllers/Product/ProductsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.Product
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public ProductsController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<BProduct> GetBProduct()
+        {
+            return _context.BProduct;
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBProduct([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProduct = await _context.BProduct.FindAsync(id);
+
+            if (bProduct == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bProduct);
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBProduct([FromRoute] int id, [FromBody] BProduct bProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bProduct.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bProduct).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<IActionResult> PostBProduct([FromBody] BProduct bProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BProduct.Add(bProduct);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBProduct", new { id = bProduct.ProductId }, bProduct);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBProduct([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProduct = await _context.BProduct.FindAsync(id);
+            if (bProduct == null)
+            {
+                return NotFound();
+            }
+
+            _context.BProduct.Remove(bProduct);
+            await _context.SaveChangesAsync();
+
+            return Ok(bProduct);
+        }
+
+        private bool BProductExists(int id)
+        {
+            return _context.BProduct.Any(e => e.ProductId == id);
+        }
+    }
+}
