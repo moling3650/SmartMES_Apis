d0092dd965687ea2552a5a8667f76a275867e848
moling <365024424@qq.com>
2018-11-06 14:35:35 +0800

    添加模拟量和管制项的API

---
+++ SmartMES_Apis/Controllers/Machine/MachineAnalogPointsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.Machine
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class MachineAnalogPointsController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public MachineAnalogPointsController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MachineAnalogPoints
+        [HttpGet]
+        public IEnumerable<BMachineAnalogPoint> GetBMachineAnalogPoint([FromQuery] string machineCode)
+        {
+            if (!String.IsNullOrWhiteSpace(machineCode))
+            {
+                return _context.BMachineAnalogPoint.Where(e => e.MachineCode.Equals(machineCode));
+            }
+            return _context.BMachineAnalogPoint;
+        }
+
+        // GET: api/MachineAnalogPoints/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBMachineAnalogPoint([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bMachineAnalogPoint = await _context.BMachineAnalogPoint.FindAsync(id);
+
+            if (bMachineAnalogPoint == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bMachineAnalogPoint);
+        }
+
+        [HttpGet("Validate")]
+        public bool ValidateBMachineStandardPoint([FromQuery] string machineCode, [FromQuery] string businessCode)
+        {
+            if (String.IsNullOrWhiteSpace(machineCode) || String.IsNullOrWhiteSpace(businessCode))
+            {
+                return false;
+            }
+            return !_context.BMachineAnalogPoint.Any(p => p.MachineCode.Equals(machineCode) && p.BusinessCode.Equals(businessCode));
+        }
+
+        // PUT: api/MachineAnalogPoints/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBMachineAnalogPoint([FromRoute] int id, [FromBody] BMachineAnalogPoint bMachineAnalogPoint)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bMachineAnalogPoint.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bMachineAnalogPoint).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BMachineAnalogPointExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MachineAnalogPoints
+        [HttpPost]
+        public async Task<IActionResult> PostBMachineAnalogPoint([FromBody] BMachineAnalogPoint bMachineAnalogPoint)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BMachineAnalogPoint.Add(bMachineAnalogPoint);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBMachineAnalogPoint", new { id = bMachineAnalogPoint.Id }, bMachineAnalogPoint);
+        }
+
+        // DELETE: api/MachineAnalogPoints/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBMachineAnalogPoint([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bMachineAnalogPoint = await _context.BMachineAnalogPoint.FindAsync(id);
+            if (bMachineAnalogPoint == null)
+            {
+                return NotFound();
+            }
+
+            _context.BMachineAnalogPoint.Remove(bMachineAnalogPoint);
+            await _context.SaveChangesAsync();
+
+            return Ok(bMachineAnalogPoint);
+        }
+
+        private bool BMachineAnalogPointExists(int id)
+        {
+            return _context.BMachineAnalogPoint.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SmartMES_Apis/Controllers/ProcessFlow/ProcessControlItemsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.ProcessFlow
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class ProcessControlItemsController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public ProcessControlItemsController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProcessControlItems
+        [HttpGet]
+        public IEnumerable<BProcessControlItem> GetBProcessControlItem([FromQuery] string processCode)
+        {
+            if (!String.IsNullOrWhiteSpace(processCode))
+            {
+                return _context.BProcessControlItem.Where(e => e.ProcessCode.Equals(processCode));
+            }
+            return _context.BProcessControlItem;
+        }
+
+        // GET: api/ProcessControlItems/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBProcessControlItem([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProcessControlItem = await _context.BProcessControlItem.FindAsync(id);
+
+            if (bProcessControlItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bProcessControlItem);
+        }
+
+        // PUT: api/ProcessControlItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBProcessControlItem([FromRoute] int id, [FromBody] BProcessControlItem bProcessControlItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bProcessControlItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bProcessControlItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BProcessControlItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProcessControlItems
+        [HttpPost]
+        public async Task<IActionResult> PostBProcessControlItem([FromBody] BProcessControlItem bProcessControlItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BProcessControlItem.Add(bProcessControlItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBProcessControlItem", new { id = bProcessControlItem.Id }, bProcessControlItem);
+        }
+
+        // DELETE: api/ProcessControlItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBProcessControlItem([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProcessControlItem = await _context.BProcessControlItem.FindAsync(id);
+            if (bProcessControlItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.BProcessControlItem.Remove(bProcessControlItem);
+            await _context.SaveChangesAsync();
+
+            return Ok(bProcessControlItem);
+        }
+
+        private bool BProcessControlItemExists(int id)
+        {
+            return _context.BProcessControlItem.Any(e => e.Id == id);
+        }
+    }
+}
