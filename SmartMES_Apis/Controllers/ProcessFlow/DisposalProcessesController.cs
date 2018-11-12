c356400b7dd43d42260c56053bf405e59a77012b
moling <365024424@qq.com>
2018-11-12 18:17:12 +0800

    添加工艺模块的API

---
+++ SmartMES_Apis/Controllers/ProcessFlow/DisposalProcessesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.ProcessFlow
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class DisposalProcessesController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public DisposalProcessesController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DisposalProcesses
+        [HttpGet]
+        public IEnumerable<BDisposalProcess> GetBDisposalProcess()
+        {
+            return _context.BDisposalProcess;
+        }
+
+        // GET: api/DisposalProcesses/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBDisposalProcess([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bDisposalProcess = await _context.BDisposalProcess.FindAsync(id);
+
+            if (bDisposalProcess == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bDisposalProcess);
+        }
+
+        // PUT: api/DisposalProcesses/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBDisposalProcess([FromRoute] int id, [FromBody] BDisposalProcess bDisposalProcess)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bDisposalProcess.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bDisposalProcess).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BDisposalProcessExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DisposalProcesses
+        [HttpPost]
+        public async Task<IActionResult> PostBDisposalProcess([FromBody] BDisposalProcess bDisposalProcess)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BDisposalProcess.Add(bDisposalProcess);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBDisposalProcess", new { id = bDisposalProcess.Id }, bDisposalProcess);
+        }
+
+        // DELETE: api/DisposalProcesses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBDisposalProcess([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bDisposalProcess = await _context.BDisposalProcess.FindAsync(id);
+            if (bDisposalProcess == null)
+            {
+                return NotFound();
+            }
+
+            _context.BDisposalProcess.Remove(bDisposalProcess);
+            await _context.SaveChangesAsync();
+
+            return Ok(bDisposalProcess);
+        }
+
+        private bool BDisposalProcessExists(int id)
+        {
+            return _context.BDisposalProcess.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SmartMES_Apis/Controllers/ProcessFlow/ProcessStepsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.ProcessFlow
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class ProcessStepsController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public ProcessStepsController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProcessSteps
+        [HttpGet]
+        public IEnumerable<BProcessStep> GetBProcessStep([FromQuery] int? pid) 
+        {
+            if (pid != null)
+            {
+                return _context.BProcessStep.AsNoTracking().Where(e => e.Pid == pid);
+            }
+            return _context.BProcessStep.AsNoTracking();
+        }
+
+        // GET: api/ProcessSteps/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBProcessStep([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProcessStep = await _context.BProcessStep.FindAsync(id);
+
+            if (bProcessStep == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bProcessStep);
+        }
+
+        // PUT: api/ProcessSteps/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBProcessStep([FromRoute] int id, [FromBody] BProcessStep bProcessStep)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bProcessStep.StepId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bProcessStep).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BProcessStepExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProcessSteps
+        [HttpPost]
+        public async Task<IActionResult> PostBProcessStep([FromBody] BProcessStep bProcessStep)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BProcessStep.Add(bProcessStep);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBProcessStep", new { id = bProcessStep.StepId }, bProcessStep);
+        }
+
+        // DELETE: api/ProcessSteps/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBProcessStep([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProcessStep = await _context.BProcessStep.FindAsync(id);
+            if (bProcessStep == null)
+            {
+                return NotFound();
+            }
+
+            _context.BProcessStep.Remove(bProcessStep);
+            await _context.SaveChangesAsync();
+
+            return Ok(bProcessStep);
+        }
+
+        private bool BProcessStepExists(int id)
+        {
+            return _context.BProcessStep.Any(e => e.StepId == id);
+        }
+    }
+}
