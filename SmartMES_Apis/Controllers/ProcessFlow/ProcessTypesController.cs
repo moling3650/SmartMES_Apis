572de22d8351bd51dd7038f64047668a793d47fc
moling <365024424@qq.com>
2018-12-04 17:48:38 +0800

    添加新的API

---
+++ SmartMES_Apis/Controllers/ProcessFlow/ProcessTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.ProcessFlow
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class ProcessTypesController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public ProcessTypesController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProcessTypes
+        [HttpGet]
+        public IEnumerable<BProcessType> GetBProcessType()
+        {
+            return _context.BProcessType;
+        }
+
+        // GET: api/ProcessTypes/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBProcessType([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProcessType = await _context.BProcessType.FindAsync(id);
+
+            if (bProcessType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bProcessType);
+        }
+
+        // PUT: api/ProcessTypes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBProcessType([FromRoute] int id, [FromBody] BProcessType bProcessType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bProcessType.TypeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bProcessType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BProcessTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProcessTypes
+        [HttpPost]
+        public async Task<IActionResult> PostBProcessType([FromBody] BProcessType bProcessType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BProcessType.Add(bProcessType);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBProcessType", new { id = bProcessType.TypeId }, bProcessType);
+        }
+
+        // DELETE: api/ProcessTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBProcessType([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bProcessType = await _context.BProcessType.FindAsync(id);
+            if (bProcessType == null)
+            {
+                return NotFound();
+            }
+
+            _context.BProcessType.Remove(bProcessType);
+            await _context.SaveChangesAsync();
+
+            return Ok(bProcessType);
+        }
+
+        private bool BProcessTypeExists(int id)
+        {
+            return _context.BProcessType.Any(e => e.TypeId == id);
+        }
+    }
+}
---
+++ SmartMES_Apis/Controllers/ProcessFlow/WorkGroupClassesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.ProcessFlow
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class WorkGroupClassesController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public WorkGroupClassesController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/WorkGroupClasses
+        [HttpGet]
+        public IEnumerable<BWorkGroupClass> GetBWorkGroupClass([FromQuery] string groupCode)
+        {
+            if (!String.IsNullOrWhiteSpace(groupCode))
+            {
+                return _context.BWorkGroupClass.Where(e => e.GroupCode.Equals(groupCode));
+            }
+            return _context.BWorkGroupClass;
+        }
+
+        // GET: api/WorkGroupClasses/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBWorkGroupClass([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bWorkGroupClass = await _context.BWorkGroupClass.FindAsync(id);
+
+            if (bWorkGroupClass == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bWorkGroupClass);
+        }
+
+        // PUT: api/WorkGroupClasses/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBWorkGroupClass([FromRoute] int id, [FromBody] BWorkGroupClass bWorkGroupClass)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bWorkGroupClass.Cid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bWorkGroupClass).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BWorkGroupClassExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/WorkGroupClasses
+        [HttpPost]
+        public async Task<IActionResult> PostBWorkGroupClass([FromBody] BWorkGroupClass bWorkGroupClass)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BWorkGroupClass.Add(bWorkGroupClass);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBWorkGroupClass", new { id = bWorkGroupClass.Cid }, bWorkGroupClass);
+        }
+
+        // DELETE: api/WorkGroupClasses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBWorkGroupClass([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bWorkGroupClass = await _context.BWorkGroupClass.FindAsync(id);
+            if (bWorkGroupClass == null)
+            {
+                return NotFound();
+            }
+
+            _context.BWorkGroupClass.Remove(bWorkGroupClass);
+            await _context.SaveChangesAsync();
+
+            return Ok(bWorkGroupClass);
+        }
+
+        private bool BWorkGroupClassExists(int id)
+        {
+            return _context.BWorkGroupClass.Any(e => e.Cid == id);
+        }
+    }
+}
