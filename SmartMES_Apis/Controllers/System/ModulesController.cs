34879185a4479d97050781bc12a16bc43a00d3d9
moling <365024424@qq.com>
2018-12-19 15:54:15 +0800

    添加新的系统接口

---
+++ SmartMES_Apis/Controllers/System/ModulesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.System
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class ModulesController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public ModulesController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Modules
+        [HttpGet]
+        public IEnumerable<SModule> GetSModule()
+        {
+            return _context.SModule;
+        }
+
+        // GET: api/Modules/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSModule([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var sModule = await _context.SModule.FindAsync(id);
+
+            if (sModule == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sModule);
+        }
+
+        // PUT: api/Modules/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSModule([FromRoute] int id, [FromBody] SModule sModule)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sModule.ModuleCode)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sModule).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SModuleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Modules
+        [HttpPost]
+        public async Task<IActionResult> PostSModule([FromBody] SModule sModule)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.SModule.Add(sModule);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSModule", new { id = sModule.ModuleCode }, sModule);
+        }
+
+        // DELETE: api/Modules/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSModule([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var sModule = await _context.SModule.FindAsync(id);
+            if (sModule == null)
+            {
+                return NotFound();
+            }
+
+            _context.SModule.Remove(sModule);
+            await _context.SaveChangesAsync();
+
+            return Ok(sModule);
+        }
+
+        private bool SModuleExists(int id)
+        {
+            return _context.SModule.Any(e => e.ModuleCode == id);
+        }
+    }
+}
---
+++ SmartMES_Apis/Controllers/System/RolesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartMES_Apis.Models;
+
+namespace SmartMES_Apis.Controllers.System
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigin")]
+    public class RolesController : ControllerBase
+    {
+        private readonly dbContext _context;
+
+        public RolesController(dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Roles
+        [HttpGet]
+        public IEnumerable<SRole> GetSRole()
+        {
+            return _context.SRole;
+        }
+
+        // GET: api/Roles/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSRole([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var sRole = await _context.SRole.FindAsync(id);
+
+            if (sRole == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sRole);
+        }
+
+        // api/Roles/Validate?roleId=code
+        [HttpGet("Validate")]
+        public bool Validate([FromQuery] string roleId)
+        {
+            if (String.IsNullOrWhiteSpace(roleId))
+            {
+                return false;
+            }
+            return !_context.SRole.Any(e => e.RoleId.Equals(roleId));
+        }
+
+        // PUT: api/Roles/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSRole([FromRoute] int id, [FromBody] SRole sRole)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sRole.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sRole).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SRoleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Roles
+        [HttpPost]
+        public async Task<IActionResult> PostSRole([FromBody] SRole sRole)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.SRole.Add(sRole);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSRole", new { id = sRole.Id }, sRole);
+        }
+
+        // DELETE: api/Roles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSRole([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var sRole = await _context.SRole.FindAsync(id);
+            if (sRole == null)
+            {
+                return NotFound();
+            }
+
+            _context.SRole.Remove(sRole);
+            await _context.SaveChangesAsync();
+
+            return Ok(sRole);
+        }
+
+        private bool SRoleExists(int id)
+        {
+            return _context.SRole.Any(e => e.Id == id);
+        }
+    }
+}
